048932dcdfcd967ee5375517cc44fe9520b9a3c0
apenfe <adrian1414@hotmail.es>
2024-05-11 09:51:54 +0200

    flujo ejecucion

---
+++ tercera_flujoEjecucion.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf=8">
+    <title>Documento sin titulo</title>
+</head>
+
+<body>
+
+<?php
+
+echo "Este es el primer mensaje <br>";
+
+    function dameDatos(){
+
+        echo "Este es el mensaje del interior de la funcion<br>";
+
+    }
+
+    dameDatos();
+
+echo "Este es el segundo mensaje <br>";
+
+?>
+
+</body>
+</html>
