0d6d0522c5336cf94f0e183730690dad0c486ad0
apenfe <adrian1414@hotmail.es>
2024-05-11 09:57:13 +0200

    flujo ejecucion

---
+++ funciones.php
@@ -0,0 +1,9 @@
+<?php
+
+function dameDatos(){
+
+    echo "Este es el mensaje del interior de la funcion<br>";
+
+}
+
+?>
--- tercera_flujoEjecucion.php
+++ tercera_flujoEjecucion.php
@@ -8,14 +8,13 @@
 <body>
 
 <?php
+include ("funciones.php");
+    dameDatos();
+?>
+
+<?php
 
 echo "Este es el primer mensaje <br>";
-
-    function dameDatos(){
-
-        echo "Este es el mensaje del interior de la funcion<br>";
-
-    }
 
     dameDatos();
 
