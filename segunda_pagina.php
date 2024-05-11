f38daa524c16a6b6b86a4c935bbc231207df3abf
apenfe <adrian1414@hotmail.es>
2024-05-11 09:24:50 +0200

    comentarios, variables

---
+++ segunda_pagina.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf=8">
+    <title>Documento sin titulo</title>
+</head>
+
+<body>
+
+<?php
+
+echo "bienvenidos al curso de php <br>"; // imprime y hace un salto de linea <br>
+
+echo "hola alumnos <br>";
+
+echo "hasta el proximo video";
+
+?>
+
+</body>
+</html>
