0863e7342797221f53b5b0ea447f751f8d4e9cf8
apenfe <adrian1414@hotmail.es>
2024-05-14 18:14:18 +0200

    ambito de las variables

---
+++ ambito_variables.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf=8">
+    <title>Documento sin titulo</title>
+</head>
+
+<body>
+
+<?php
+
+$nombre = "Juan";
+
+function dameNombre(){
+    global $nombre;
+
+    $nombre = "mi nombre es ". $nombre;
+}
+
+dameNombre();
+echo $nombre ;
+
+?>
+
+</body>
+</html>
