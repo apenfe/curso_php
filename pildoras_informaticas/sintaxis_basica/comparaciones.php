8cc7ab0d0f21f36b1551aa9beec5c7a01778fef6
apenfe <adrian1414@hotmail.es>
2024-05-16 08:40:47 +0200

    operadores comparacion

---
+++ pildoras_informaticas/sintaxis_basica/comparaciones.php
@@ -0,0 +1,29 @@
+<?php
+
+$variable1 = 8;
+$variable2 ="8";
+$variable3 ="juan";
+
+if($variable1==$variable2){
+    echo "son iguales";
+}else{
+    echo "no son iguales";
+}
+
+echo "<br>";
+
+if($variable1===$variable2){
+    echo "son iguales";
+}else{
+    echo "no son iguales";
+}
+
+echo "<br>";
+
+if($variable1!=$variable3){
+    echo "son diferentes";
+}else{
+    echo "no son diferentes";
+}
+
+?>
