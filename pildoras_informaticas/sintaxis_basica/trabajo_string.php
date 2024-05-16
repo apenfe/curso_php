53ab8ba36746eb9efad8c4ca0d24355613b1eff7
apenfe <adrian1414@hotmail.es>
2024-05-16 08:15:47 +0200

    strings

--- pildoras_informaticas/sintaxis_basica/trabajo_string.php
+++ pildoras_informaticas/sintaxis_basica/trabajo_string.php
@@ -17,7 +17,26 @@
 
 <?php
 
+    $nombre = "adrian";
+    echo "hola $nombre";
     echo "<p class='resaltar'>Esto es un ejemplo de frase</p>";
+
+    // comparaciones de string
+
+$variable1 = "casa";
+$variable2 = "CASA";
+
+$resultado =strcmp($variable1,$variable2); // devuelve 0 si son iguales y 1 si no lo son
+$resultado2 =strcasecmp($variable1,$variable2); // devuelve 0 si son iguales y 1 si no lo son
+
+echo "el resultado sin case es: $resultado <br>";
+echo "el resultado con case es: $resultado2 <br>";
+
+if(!$resultado2){
+    echo "coinciden";
+}else{
+    echo "no coinciden";
+}
 
 ?>
 
