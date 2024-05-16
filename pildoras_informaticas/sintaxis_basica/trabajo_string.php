61c7bf163ba22c7d81c87e446232f828bb90d213
apenfe <adrian1414@hotmail.es>
2024-05-16 08:04:21 +0200

    strings

---
+++ pildoras_informaticas/sintaxis_basica/trabajo_string.php
@@ -0,0 +1,25 @@
+<!doctype html>
+
+<html>
+<head>
+    <meta charset="utf=8">
+    <title>Documento sin titulo</title>
+    <style>
+        .resaltar{
+            color:#F00;
+            font-weight:bold;
+        }
+    </style>
+
+</head>
+
+<body>
+
+<?php
+
+    echo "<p class='resaltar'>Esto es un ejemplo de frase</p>";
+
+?>
+
+</body>
+</html>
