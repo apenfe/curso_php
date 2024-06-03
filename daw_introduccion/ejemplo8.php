855768e2962d89d7e38eed2fb062575bf6973390
apenfe <adrian1414@hotmail.es>
2024-06-03 17:08:25 +0200

    Primer formulario en php

---
+++ daw_introduccion/ejemplo8.php
@@ -0,0 +1,34 @@
+<html>
+
+    <head>
+
+        <title>Ejemplo 8 (arrays)</title>
+
+    </head>
+
+    <body>
+
+        <?php
+
+        function mostrarArray($cosas) {
+            $texto = "<h3> listado de las ".count($cosas)." cosas.</h3>";
+
+            $texto.= "<ul>";
+
+            for ($i = 0; $i < count($cosas); $i++) {
+                $texto .= "<li>".$cosas[$i]."</li>";
+            }
+
+            $texto.= "</ul>";
+            return $texto;
+        }
+
+        $cosas = array("Chocolate",42,true,"23",34.44);
+
+        echo mostrarArray($cosas);
+
+        ?>
+
+    </body>
+
+</html>
