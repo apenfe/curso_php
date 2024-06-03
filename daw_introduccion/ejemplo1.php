b7804c7adda861536fdd0dc477788717e4dc1b6d
apenfe <adrian1414@hotmail.es>
2024-06-03 15:51:36 +0200

    comienzo preparacion php daw

---
+++ daw_introduccion/ejemplo1.php
@@ -0,0 +1,14 @@
+<?php
+
+echo '<html> ';
+
+echo '<head> ';
+echo '<title>Ejemplo 1 PHP</title>';
+echo '</head> ';
+
+echo '<body> ';
+echo '<h1>Ejemplo 1 EN PHP</h1> ';
+echo '</body> ';
+
+echo '</html> ';
+?>
---
+++ daw_introduccion/ejemplo1b.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+
+    <?php
+        echo '<title>Ejemplo 1</title> ';
+    ?>
+
+</head>
+<body>
+
+    <?php
+        echo '<h1>Ejemplo 1 EN (PHP)</h1> ';
+    ?>
+
+</body>
+</html>
