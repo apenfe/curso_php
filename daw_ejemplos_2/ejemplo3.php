212af72087559cfa83cb9d161856e1b65dd0bf06
apenfe <adrian1414@hotmail.es>
2024-06-03 18:03:24 +0200

    Tercer formulario en php

--- daw_ejemplos_2/ejemplo3.php
+++ daw_ejemplos_2/ejemplo3.php
@@ -12,19 +12,27 @@
 
 <?php
 
-$num = $_POST["numero"];
-echo "<h2> Tabla del numero $num </h2>";
+if(isset($_POST["numero"])){
 
-$i = 1;
+    $num = $_POST["numero"];
+    echo "<h2> Tabla del numero $num </h2>";
 
-if($num<1 || $num>10){
-    echo "<h2 style='color: #FF0000'> Tabla no valida </h2>";
+    $i = 1;
+
+    if($num<1 || $num>10){
+        echo "<h2 style='color: #FF0000'> Tabla no valida </h2>";
+    }else{
+
+        while($i <= 10){
+            echo $i ."x". $num ."= ".($i*$num)."<br>";
+            $i++;
+        }
+
+    }
+
 }else{
 
-    while($i <= 10){
-        echo $i ."x". $num ."= ".($i*$num)."<br>";
-        $i++;
-    }
+    echo "<h2 style='color: #FF0000'> Es necesario cargar un nuemro para calcular la tabla </h2>";
 
 }
 
